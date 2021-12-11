46ded1a3c6af1b95367127324abf24521c9e96a9
hubertpawlak <87566321+hubertpawlak@users.noreply.github.com>
2021-12-11 12:53:51 +0100

    Show useful help

--- src/modules/showhelp.c
+++ src/modules/showhelp.c
@@ -1,12 +1,23 @@
 #include "showhelp.h"
 
+const char rawFormat[] = "  %%-%ds  %%s\n";
+
 void showHelp(bool argsAreInvalid)
 {
+  char *format = calloc(strlen(rawFormat), sizeof(char));
   if (argsAreInvalid)
   {
     printf("Provided arguments are invalid.\n");
   }
   printf("Usage: ./program [OPTIONS]\n");
-  // TODO: Show available options
+  printf("Options must be separate ex. \"-a -b -c\" not \"-abc\"\n");
+  sprintf(format, rawFormat, HELP_PADDING);
+  printf(format, "-h, --help", "Show how to use this program");
+  printf(format, "-i, --input <file>", "Convert this file");
+  printf(format, "-o, --output <file>", "Override default output filename");
+  printf(format, "-f, --force", "Overwrite output file");
+  printf(format, "--md", "Treat input file as Markdown");
+  printf(format, "--html", "Treat input file as HTML");
   printf("Example: ./program -i file.md -o file.html\n");
+  free(format);
 }
--- src/modules/showhelp.h
+++ src/modules/showhelp.h
@@ -2,5 +2,8 @@
 #include <stdio.h>
 #include <stdbool.h>
 #include <string.h>
+#include <stdlib.h>
+
+#define HELP_PADDING 20
 
 void showHelp(bool argsAreInvalid);
